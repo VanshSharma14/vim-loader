7f997270e521cd03b9772d181c4049fca410e386
VanshSharma14 <vansh.business14@gmail.com>
2022-08-05 21:52:09 -0400

    fixed init file

--- init.sh
+++ init.sh
@@ -2,14 +2,19 @@
 
 # replace original vimrc with updated vimrc
 cp .vimrc ~
-
+# CD to home directory
 cd ~
-
+# Get Vim-Plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Get Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Run Vim commands
 vim --cmd '' \
        -c 'PlugInstall' \
        -c 'PluginInstall' \
        -c 'qa!' # Quit vim
-
-source .vimrc
+# Source the VIM file
+source ~/.vimrc
 clear
 cd
